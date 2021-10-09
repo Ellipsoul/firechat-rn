99a88436abbf0a97630a55fc1cb845f7b74b562e
Aron Teh <aronteh.chess@gmail.com>
2021-10-09 14:40:35 +0100

    Blank components and screens initialised and home screen implemented

--- App.js
+++ App.js
@@ -1,111 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, StatusBar, View, StyleSheet} from 'react-native';
+import ChatScreen from './screens/chatScreen';
+import auth from '@react-native-firebase/auth';
+import LoginScreen from './screens/loginScreen';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  // Track the user's authentication state
+  const [user, setUser] = useState(null);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  // Run the Firebase authentication on the user just once
+  useEffect(() => {
+    // onAuthStateChanged is an auth listener
+    const unsubscribe = auth().onAuthStateChanged(u => {
+      u ? setUser(u) : setUser(null); // Set the user if authenticated, otherwise leave null
+    });
+    // Returns a function that you can use to turn off the realtime stream when component is destroyed
+    return () => {
+      unsubscribe();
+    };
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    // SafeAreaView means the component will not be blocked by any device notches
+    <SafeAreaView style={styles.backgroundStyle}>
+      <StatusBar barStyle={'dark-content'} />
+      {/* Container showing chat or login depending on if the user is logged in */}
+      <View style={styles.viewStyle}>
+        {user ? <ChatScreen /> : <LoginScreen />}
+      </View>
     </SafeAreaView>
   );
 };
 
+// Simple styles
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  backgroundStyle: {
+    backgroundColor: '#151718',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  viewStyle: {
+    height: '100%',
+    alignItems: 'center',
+    backgroundColor: '#151718',
   },
 });
 
